6263c49a7927f8883571ae0efd21bffc70421731
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2023-08-12 02:10:14 +0900

    make li elements from a JSON string

--- docs/main.js
+++ docs/main.js
@@ -2,6 +2,23 @@
   const ulElem = document.querySelector("ul");
   const dialogElem = document.querySelector("dialog");
   const answerElem = document.querySelector("dialog > .answer");
+
+  const json_str = JSON.stringify([{question: "foo", answer: "baz"}, {question: "question", answer: "answer"}]);
+  const json = JSON.parse(json_str);
+
+  liFragment = new DocumentFragment();
+  json.forEach(({question, answer}) => {
+    pElem = document.createElement("p");
+    pElem.innerText = question;
+    pElem.className = "question";
+
+    liElem = document.createElement("li");
+    liElem.append(pElem);
+
+    liFragment.append(liElem);
+  });
+  ulElem.append(liFragment);
+
   const flipper = {
     "foo": "baz",
     "question": "answer"
